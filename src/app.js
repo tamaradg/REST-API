55a59302e0541958d137a293c1f82e7d04acf54f
Tamara de Gaetano <tamara.degaetano@yahoo.com>
2022-09-02 17:24:58 +0200

    add middleware methods validate

--- src/app.js
+++ src/app.js
@@ -1,57 +1,109 @@
+const { response } = require("express");
 const express = require("express");
+
 // Catégories :  /api/categories
 // Produits : /api/products
+
 const categories = [
-  { id: 1, nom: "Jeux vidéos" },
-  { id: 2, nom: "Ordinateurs" },
+  { id: 1, nom: "Jeux vidéos", description:"Super" },
+  { id: 2, nom: "Ordinateurs", description:"Génial" },
 ];
+
 const app = express();
-//on ajoute le middleware pour post ici
+// le middleware
 app.use(express.json());
+
+function validateCategory(request, response, next) {
+  console.log("Je suis le middleware");
+  const id = parseInt(request.params.id);
+  const category = categories.find((category) => category.id === parseInt(id));
+  if (!category) {
+    return response.sendStatus(404);
+  }
+  request.category = category;
+  next();
+}
+
+function validateUpdate(req, res, next) {
+  if (!req.body.nom && !req.body.description) {
+    return res.status(400).send('Le nom et la description doivent être renseignés');
+  } else if (!req.body.description) {
+    return res.status(400).send('La decription ne peut pas être vide');
+  } else if (!req.body.nom) {
+    return res.status(400).send("Le nom ne peut pas être vide");
+  }
+  next();
+};
+
+function log(request,response, next) {
+  let method = request.method;
+  let url = request.url;
+  let bodies = JSON.stringify(request.body);
+  console.log(`${method} ${url} , BODY ${bodies}`);
+  next();
+}
+
 app.get("/api/categories", (request, response) => {
   response.send(categories);
 });
 
-app.get("/api/categories/:id", (request, response) => {
-  const id = parseInt(request.params.id);
-  //   const category = categories.find(function (category) {
-  //     return category.id === parseInt(id);
-  //   });
-  const category = categories.find((category) => category.id === parseInt(id));
-  response.send(category);
+app.get("/api/categories/:id", validateCategory, (request, response) => {
+  response.send(request.category);
 });
 
-app.post("/api/categories", (request, response) => {
+app.post("/api/categories", log, (request, response) => {
   categories.push(request.body);
   response.send("Catégorie créée avec succès");
 });
 
-app.put('/api/categories/:id', (req, res) => {
-    const id = ParseInt(req.params.id);
-    const index = products.findIndex(category => category.id === id)
-    // if (index === -1) {
-    //     return res.status(404).send('Product not found')
-    // }
-    const updatedCat = {
-        id: categories[index].id,
-        name: req.body.nom
-    }
-    categories[index] = updatedCat
-    res.status(200).json('Category updated')
+app.delete("/api/categories/:id", validateCategory, (request, response) => {
+  const categoryIndex = categories.indexOf(request.category);
+  categories.splice(categoryIndex, 1);
+  response.send("Supprimé avec succès");
 });
 
-app.delete('/api/categories/:id', (req, res) => {
-    const id = ParseInt(req.params.id);
-    const index = products.findIndex(category => category.id === id)
-    //     if (index === -1) {
-    //     return res.status(404).send('Product not found')
-    // }
-    categories.splice(index,1)
-    res.status(200).json('Category deleted')
-})
+app.put("/api/categories/:id", validateCategory, validateUpdate, (request, response) => {
+  Object.assign(request.category, request.body);
+  response.send("Catégorie mis à jour ave c succès");
+});
+
+// #1. Ajouter un champs description dans les catégories
+app.patch("/api/categories/:id", validateCategory, (request, response) => {
+  Object.assign(request.category, request.body);
+  response.send("Catégorie mis à jour avec succès");
+});
+
+// #2. Ajouter une logique de validation pour la création et mis à jour des categories
+// #3. Si le nom est vide, écrire : "Le nom ne peut pas être vide"
+// #4. Si la description est vide, écrire : "La description ne peut pas être vide"
+// #5. En cas d'erreur, renvoyer un objet d'erreur
+//    exemple :
+// Si seul le nom est vide, renvoyer : body :{description:"effwfewf"}
+// {
+  //   errors: {
+  //     nom: "Le nom ne peut pas être vide",
+  //   }
+  // }
+  // Si seul la description est vide, renvoyer  : body {nom:"sdffdsf"}
+  // {
+    //   errors: {
+      //     description: "La description ne peut pas être vide",
+      //   }
+      // }
+      // Si les 2 sont vides:
+  // {
+    //   errors: {
+      //     nom: "Le nom ne peut pas être vide",
+      //     description: "La description ne peut pas être vide",
+      //   }
+      // }
+// Ne pas envoyer un id.
+// L'id est généré à partir d'un nombre aléatoire entre 1 et 999(Math.random)
+
+
 
 const PORT = 3000;
+
 app.listen(PORT, () => {
   console.log(`Server run on port ${PORT}`);
 });
-//CRUD (Create Read Update Delete)
